9d5232045e0f19880466e3aef678aac2b3c37860
S-L-I-G-H-T-N-I-N-G <3157420882@qq.com>
2022-01-16 16:59:38 +0800

    菜单栏动画（未完）

--- top-bar/menu.js
+++ top-bar/menu.js
@@ -1,11 +1,24 @@
 var lastWidth;
 
-function showMenu() {
+function onClickMenuButton() {
+    //var menu_out_bar = document.getElementById("menu-out-bar");
+    //if (menu_out_bar.style.display == "block")
+    //    menu_out_bar.style.display = "none";
+    //else
+    //    menu_out_bar.style.display = "block";
     var menu_out_bar = document.getElementById("menu-out-bar");
-    if (menu_out_bar.style.display == "block")
-        menu_out_bar.style.display = "none";
-    else
-        menu_out_bar.style.display = "block";
+    openMemu();
+    if (menu_out_bar.style.bottom == 0)
+        menu_out_bar.style.animationDuration = "reverse";
+}
+
+function openMemu() {
+    var menu_out_bar = document.getElementById("menu-out-bar");
+    menu_out_bar.style.animationName = null;
+    menu_out_bar.style.animationName = "open-menu";
+	menu_out_bar.style.animationDuration = "1s";
+    menu_out_bar.style.animationIterationCount = 1;
+    menu_out_bar.style.animationFillMode = "forwards";
 }
 
 function watchWindowSize() {
@@ -17,7 +30,7 @@ function watchWindowSize() {
     else if (width < 600 && 600 <= lastWidth) {
         menu_out_bar = document.getElementById("menu-out-bar")
         menu_out_bar.appendChild(document.getElementById("menu"));
-        menu_out_bar.style.display = "none"
+        //menu_out_bar.style.display = "none"
     }
     lastWidth = width;
 }
