82042aa2df413d0fbbc9839241bbfbced6aa9538
S-L-I-G-H-T-N-I-N-G <89953982+S-L-I-G-H-T-N-I-N-G@users.noreply.github.com>
2022-02-25 19:49:45 +0800

    优化菜单、主页

--- js/top-bar--menu.js
+++ js/top-bar--menu.js
@@ -1,6 +1,5 @@
-var menuNutton = document.getElementById("top-bar--menu")
+var menu = document.getElementById("top-bar--menu")
 var navigationBar = document.getElementById("top-bar--navigation-bar")
-var menuAnimationStartTime
 
 function onClickTopBarMenuButton() {
     if (topBarMenuIsOpen()) {
@@ -13,19 +12,13 @@ function topBarMenuIsOpen() {
     return navigationBar.style.top == "4rem"
 }
 function openTopBarMenu() {
-	menuAnimationStartTime = new Date().getTime()
-	menuNutton.style.height = "calc(100vh - 4rem)"
-    menuNutton.style.background = "#00000088"
+    menu.style.pointerEvents = "auto"
+    menu.style.background = "#00000088"
     navigationBar.style.top = "4rem"
 }
 function closeTopBarMenu() {
-	menuAnimationStartTime = new Date().getTime()
-	var time = menuAnimationStartTime
-	setTimeout(function () {
-		if (time == menuAnimationStartTime)
-		    menuNutton.style.height = "0px"
-	}, 500)
-    menuNutton.style.background = "#00000000"
+    menu.style.pointerEvents = "none"
+    menu.style.background = "#00000000"
     navigationBar.style.top = "-4rem"
 }
 
